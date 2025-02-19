8eb18d0ec1a074c8a5b922d107a920c7e0cc5590
natalia646 <75090544+natalia646@users.noreply.github.com>
2025-02-19 20:53:42 +0200

    add all company description

--- src/components/CompanyDesc.tsx
+++ src/components/CompanyDesc.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Company } from "../types/Company.type";
 
 type Props = {
@@ -5,18 +6,24 @@ type Props = {
 };
 
 export const CompanyDesc: React.FC<Props> = ({ company }) => {
+  const { id, ...companyDescription } = company;
+
+  const fields = Object.entries(companyDescription);
 
   return (
-    <article>
-      <p><strong>ticker:</strong> {company.ticker}</p>
-      <p><strong>Name:</strong> {company.name}</p>
-      <p><strong>Legal name:</strong> {company.legal_name}</p>
-      <p><strong>Stock exchange:</strong> {company.stock_exchange}</p>
-      <p><strong>Short description:</strong> {company.short_description}</p>
-      <p><strong>Long description:</strong> {company.long_description}</p>
+    <section>
+      {fields.map((field) => {
+        const [key, value] = field;
 
-      <p><strong>Business address:</strong> {company.business_address}</p>
-      <p><strong>Business phone:</strong> {company.business_phone_no}</p>
-    </article>
+        const normalizeKey =
+          key.slice(0, 1).toUpperCase() + key.slice(1).split("_").join(" ");
+
+        return (
+          <p>
+            <strong>{normalizeKey}:</strong> {value}
+          </p>
+        );
+      })}
+    </section>
   );
 };
